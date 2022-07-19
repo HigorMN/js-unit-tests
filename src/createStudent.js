1721957200b7911a6e475ac86a4d84d647af38c5
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-07-19 17:03:35 -0300

    Requisito 5 concluido

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,15 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => {
+  let estudante = {
+    name: `${nome}, o Lobo Solitário`,
+    feedback: () => {
+      const goodPerson = 'Eita pessoa boa!';
+      return goodPerson;
+    },
+  };
+  return estudante;
+};
 
 module.exports = createStudent;
