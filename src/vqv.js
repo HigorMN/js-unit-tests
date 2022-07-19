0dd7909884575cb82f949962cd6c4761fa6492ff
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-07-19 16:00:44 -0300

    Requisito 3 concluido

--- src/vqv.js
+++ src/vqv.js
@@ -21,9 +21,11 @@
 const vqv = (name, age) => {
   if (name === undefined || age === undefined) {
     return undefined;
-  } else {
-    return `Oi, meu nome é ${name}!\nTenho ${age} anos,\ntrabalho na Trybe e mando muito em programação!\n#VQV!`;
   }
+  return `Oi, meu nome é ${name}!
+Tenho ${age} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
 };
 
 module.exports = vqv;
